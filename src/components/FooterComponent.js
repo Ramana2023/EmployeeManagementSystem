d0482d4d56a8cd49f61e283c3035ad4bc8edf28a
Ramana2023 <129718636+Ramana2023@users.noreply.github.com>
2023-06-13 22:02:10 +0530

    Add files via upload

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../App.css';
+export default class FooterComponent extends Component {
+  render() {
+    return (
+      <div>
+        <footer className='footer'>
+        <span className='text-muted'>All Reight Reserved &copy;RAMANA</span>
+        </footer>
+      </div>
+    )
+  }
+}
+ 
